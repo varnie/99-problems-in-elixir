0dd474f42e746d8d22d4d333dd56aa414831be0a
varnie <varnie29a@mail.ru>
2025-01-15 14:26:17 +0300

    code format

--- lib/Challenge56.ex
+++ lib/Challenge56.ex
@@ -1,7 +1,8 @@
 defmodule Challenge56 do
   @doc """
     (**) Symmetric binary trees
-       Let us call a binary tree symmetric if you can draw a vertical line through the root node and then the right subtree is the mirror image of the left subtree. Write a function symmetric to check whether a given binary tree is symmetric.
+       Let us call a binary tree symmetric if you can draw a vertical line through the root node and then the right subtree
+       is the mirror image of the left subtree. Write a function symmetric to check whether a given binary tree is symmetric.
        We are only interested in the structure, not in the contents of the nodes.
   """
   def is_symmetric(node) do
