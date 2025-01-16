43aee09bcfc7629207988ab19ff6bb488056d3dd
varnie <varnie29a@mail.ru>
2025-01-16 18:53:44 +0300

    add Challenge61A

---
+++ lib/Challenge61A.ex
@@ -0,0 +1,15 @@
+defmodule Challenge61A do
+  @doc """
+    (*) Collect the leaves of a binary tree in a list
+        A leaf is a node with no successors. Write a function leaves to return them in a list.
+
+        (leaves tree) returns the list of all leaves of the binary tree tree
+  """
+  def leaves(tree) do
+    cond do
+      is_nil(tree) -> []
+      TreeNode.is_leaf(tree) -> [tree]
+      true -> leaves(tree.left) ++ leaves(tree.right)
+    end
+  end
+end
