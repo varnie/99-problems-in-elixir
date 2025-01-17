787a0879c4d7853e2e87a1f68c3821315965dfe7
varnie <varnie29a@mail.ru>
2025-01-17 18:27:45 +0300

    Challenge62B added

---
+++ lib/Challenge62B.ex
@@ -0,0 +1,40 @@
+defmodule Challenge62B do
+  @doc """
+    (*) Collect the nodes at a given level in a list
+       A node of a binary tree is at level N if the path from the root to the node has length N-1.
+       The root node is at level 1. Write a function atlevel to collect all nodes at a given level in a list.
+
+       (atlevel tree l) returns the list of nodes of the binary tree tree at level L
+  """
+  def atlevel(_tree, l) when l <= 0 do
+    []
+  end
+
+  def atlevel(tree, _l) when is_nil(tree) do
+    []
+  end
+
+  def atlevel(tree, l) do
+    atlevel_helper(tree, l, 1)
+  end
+
+  defp atlevel_helper(tree, l, current_level) when current_level == l do
+    [tree]
+  end
+
+  defp atlevel_helper(tree, l, current_level) do
+    cond do
+      is_nil(tree) ->
+        []
+
+      TreeNode.is_leaf(tree) ->
+        []
+
+      true ->
+        Enum.concat(
+          atlevel_helper(tree.left, l, current_level + 1),
+          atlevel_helper(tree.right, l, current_level + 1)
+        )
+    end
+  end
+end
