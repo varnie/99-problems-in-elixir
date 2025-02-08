5f6d458c392b18b464845b1f242feb4b6b513085
varnie <varnie29a@mail.ru>
2025-02-08 22:00:13 +0300

    challenge 68 tests - refactoring

--- test/challenge68_test.exs
+++ test/challenge68_test.exs
@@ -1,89 +1,66 @@
 defmodule Challenge68Test do
   use ExUnit.Case
 
-  test "preorder" do
-    assert Challenge68.preorder(nil) == nil
-
-    assert Challenge68.preorder(%TreeNode{
-             symbol: "a",
-             weight: nil,
-             left: %TreeNode{symbol: "l", weight: nil, left: nil, right: nil},
-             right: %TreeNode{symbol: "r", weight: nil, left: nil, right: nil}
-           }) == "a l r"
-
-    assert Challenge68.preorder(%TreeNode{
-             symbol: "a",
-             weight: nil,
-             left: %TreeNode{symbol: "l", weight: nil, left: nil, right: nil},
-             right: nil
-           }) == "a l"
-
-    assert Challenge68.preorder(%TreeNode{
-             symbol: "a",
-             weight: nil,
-             right: %TreeNode{symbol: "r", weight: nil, left: nil, right: nil},
-             left: nil
-           }) == "a r"
-
-    assert Challenge68.preorder(%TreeNode{
-             symbol: "a",
-             weight: nil,
-             left: %TreeNode{
-               symbol: "b",
-               left: %TreeNode{
-                 symbol: "d"
-               },
-               right: %TreeNode{symbol: "e"}
-             },
-             right: %TreeNode{
-               symbol: "c",
-               weight: nil,
-               left: %TreeNode{symbol: "f"},
-               right: %TreeNode{symbol: "g"}
-             }
-           }) == "a b d e c f g"
+  setup_all do
+    {:ok,
+     alr_tree: %TreeNode{
+       symbol: "a",
+       weight: nil,
+       left: %TreeNode{symbol: "l", weight: nil, left: nil, right: nil},
+       right: %TreeNode{symbol: "r", weight: nil, left: nil, right: nil}
+     },
+     al_tree: %TreeNode{
+       symbol: "a",
+       weight: nil,
+       left: %TreeNode{symbol: "l", weight: nil, left: nil, right: nil},
+       right: nil
+     },
+     ar_tree: %TreeNode{
+       symbol: "a",
+       weight: nil,
+       right: %TreeNode{symbol: "r", weight: nil, left: nil, right: nil},
+       left: nil
+     },
+     some_tree: %TreeNode{
+       symbol: "a",
+       weight: nil,
+       left: %TreeNode{
+         symbol: "b",
+         left: %TreeNode{
+           symbol: "d"
+         },
+         right: %TreeNode{symbol: "e"}
+       },
+       right: %TreeNode{
+         symbol: "c",
+         weight: nil,
+         left: %TreeNode{symbol: "f"},
+         right: %TreeNode{symbol: "g"}
+       }
+     }}
   end
 
-  test "inorder" do
+  test "preorder", state do
+    assert Challenge68.preorder(nil) == nil
+
+    assert Challenge68.preorder(state[:alr_tree]) == "a l r"
+
+    assert Challenge68.preorder(state[:al_tree]) == "a l"
+
+    assert Challenge68.preorder(state[:ar_tree]) == "a r"
+
+    assert Challenge68.preorder(state[:some_tree]) == "a b d e c f g"
+  end
+
+  test "inorder", state do
     assert Challenge68.inorder(nil) == nil
 
-    assert Challenge68.inorder(%TreeNode{
-             symbol: "a",
-             weight: nil,
-             left: %TreeNode{symbol: "l", weight: nil, left: nil, right: nil},
-             right: %TreeNode{symbol: "r", weight: nil, left: nil, right: nil}
-           }) == "l a r"
+    assert Challenge68.inorder(state[:alr_tree]) == "l a r"
 
-    assert Challenge68.inorder(%TreeNode{
-             symbol: "a",
-             weight: nil,
-             left: %TreeNode{symbol: "l", weight: nil, left: nil, right: nil},
-             right: nil
-           }) == "l a"
+    assert Challenge68.inorder(state[:al_tree]) == "l a"
 
-    assert Challenge68.inorder(%TreeNode{
-             symbol: "a",
-             weight: nil,
-             right: %TreeNode{symbol: "r", weight: nil, left: nil, right: nil},
-             left: nil
-           }) == "a r"
+    assert Challenge68.inorder(state[:ar_tree]) == "a r"
 
-    assert Challenge68.inorder(%TreeNode{
-             symbol: "a",
-             weight: nil,
-             left: %TreeNode{
-               symbol: "b",
-               left: %TreeNode{
-                 symbol: "d"
-               },
-               right: %TreeNode{symbol: "e"}
-             },
-             right: %TreeNode{
-               symbol: "c",
-               weight: nil,
-               left: %TreeNode{symbol: "f"},
-               right: %TreeNode{symbol: "g"}
-             }
-           }) == "d b e a f c g"
+    assert Challenge68.inorder(state[:some_tree]) == "d b e a f c g"
   end
 end
