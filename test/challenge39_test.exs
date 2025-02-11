852a657933a9d4eb70b2415c9b7944068966d648
varnie <varnie29a@mail.ru>
2025-02-11 15:56:58 +0300

    test fixes

--- test/challenge39_test.exs
+++ test/challenge39_test.exs
@@ -2,6 +2,6 @@ defmodule Challenge39Test do
   use ExUnit.Case
 
   test "prime_numbers_list" do
-    assert Challenge39.prime_numbers_list(1, 10) == [1, 2, 3, 5, 7]
+    assert Challenge39.prime_numbers_list(1, 10) == [2, 3, 5, 7]
   end
 end
