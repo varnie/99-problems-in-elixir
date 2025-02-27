c4534508404502d0b518fc1a1433a0d7cb9034c7
varnie <varnie29a@mail.ru>
2025-02-27 15:08:02 +0300

    updates

--- test/challenge58_test.exs
+++ test/challenge58_test.exs
@@ -4,6 +4,7 @@ defmodule Challenge58Test do
   test "sym_cbal_trees" do
     assert Challenge58.sym_cbal_trees(1) == [%TreeNode{symbol: :x, left: nil, right: nil}]
     assert Challenge58.sym_cbal_trees(3) |> length == 1
+    # slow
     assert Challenge58.sym_cbal_trees(37) |> length == 256
   end
 end
