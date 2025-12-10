058fde9d020c3f11b8d35750aed8fed271caee4f
varnie <varnie29a@mail.ru>
2025-12-10 20:04:20 +0300

    Challenge93 - add tests

--- lib/challenge93.ex
+++ lib/challenge93.ex
@@ -9,11 +9,11 @@ defmodule Challenge93 do
 
   @ops [:plus, :minus, :mult, :div]
 
-  def solution(numbers) when length(numbers) < 2 do
+  def solutions(numbers) when length(numbers) < 2 do
     raise("Two ore more items required")
   end
 
-  def solution(numbers) do
+  def solutions(numbers) do
     result =
       for i <- 1..(length(numbers) - 1) do
         {x, y} = Enum.split(numbers, i)
@@ -23,12 +23,20 @@ defmodule Challenge93 do
 
         for left_comb <- left_combs,
             right_comb <- right_combs,
-            Kernel.elem(Code.eval_string("#{left_comb}==#{right_comb}"), 0) == true do
+            eval_string("#{left_comb}==#{right_comb}") do
           "#{left_comb}==#{right_comb}"
         end
       end
 
-    Enum.filter(result, &(&1 != []))
+    result |> List.flatten()
+  end
+
+  defp eval_string(val) do
+    try do
+      Kernel.elem(Code.eval_string(val), 0) == true
+    rescue
+      _ -> false
+    end
   end
 
   defp map_op_to_string(op) do
@@ -46,25 +54,24 @@ defmodule Challenge93 do
   defp gen_combs(numbers) do
     op_combs = gen_op_combs(length(numbers) - 1)
 
-    Enum.map(op_combs, fn op_comb ->
-      # we need to add dummy item to op_combs in order to use Enu.zip_with
+    Enum.map(op_combs, fn cur_op_combs ->
+      # we need to add a dummy item to cur_op_combs in order to use Enum.zip_with
       # (we need to make lengths of these sequences equal)
-      Enum.zip_with(numbers, op_comb ++ [""], fn number, op ->
+      Enum.zip_with(numbers, cur_op_combs ++ [""], fn number, op ->
         Integer.to_string(number) <> if op == "", do: "", else: map_op_to_string(op)
       end)
     end)
   end
 
-  def gen_op_combs(how_much, cur_item \\ []) do
+  def gen_op_combs(required_size, cur_item \\ []) do
     Enum.reduce(@ops, [], fn x, acc ->
       # new_cur_item is a flat list
       new_cur_item = cur_item ++ [x]
 
-      if length(new_cur_item) == how_much do
-        new_acc = [new_cur_item | acc]
-        new_acc
+      if length(new_cur_item) == required_size do
+        [new_cur_item | acc]
       else
-        Enum.concat(acc, gen_op_combs(how_much, new_cur_item))
+        Enum.concat(acc, gen_op_combs(required_size, new_cur_item))
       end
     end)
   end
---
+++ test/challenge93_test.exs
@@ -0,0 +1,21 @@
+defmodule Challenge93Test do
+  use ExUnit.Case
+
+  test "solution" do
+    assert_raise RuntimeError, fn ->
+      Challenge93.solutions([])
+    end
+
+    assert_raise RuntimeError, fn ->
+      Challenge93.solutions([1])
+    end
+
+    result = Challenge93.solutions([10, 10])
+    assert Helpers.check_two_enumerables_equal?(result, ["10==10"])
+
+    result = Challenge93.solutions([2,3,5,7,11])
+    assert Enum.member?(result, "2-3+5+7==11")
+
+    #TODO: add other tests later for ( )
+  end
+end
